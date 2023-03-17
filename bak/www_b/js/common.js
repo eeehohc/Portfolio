86799226e1ccf7eeda662221645f4adebf86a0fa
eeehohc <imega6@naver.com>
2023-03-17 17:55:33 +0900

    first commit

---
+++ bak/www_b/js/common.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    //txt_box
+
+    $(".gridback").click(function(){
+        $('.txt_box').css( "display", "none" );
+    });
+
+     $(".menu__item01 a.menu__item-explore").click(function(){
+         $('.txt_box01').css( "display", "block" );
+     });
+
+     $(".menu__item02 a.menu__item-explore").click(function(){
+         $('.txt_box02').css( "display", "block" );
+     });
+
+     $(".menu__item03 a.menu__item-explore").click(function(){
+         $('.txt_box03').css( "display", "block" );
+     });
+
+     $(".menu__item04 a.menu__item-explore").click(function(){
+        $('.txt_box04').css( "display", "block" );
+    });
+
+    $(".menu__item05 a.menu__item-explore").click(function(){
+        $('.txt_box05').css( "display", "block" );
+    });
+
+    $(".menu__item06 a.menu__item-explore").click(function(){
+        $('.txt_box06').css( "display", "block" );
+    });
+
+});
+    
